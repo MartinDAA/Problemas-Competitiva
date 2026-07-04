7e89d76947deac9fb448416824ec84bbacfe2a8d
MartinDAA <martin.alonso.personal@gmail.com>
2026-07-04 15:34:40 -0500

    estructura inicial

---
+++ kmp/1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+using ll = long long;
+
+const bool HAS_CASE = true; //true if t (test cases) is part of input, false otherwise
+
+void solve(){
+
+}
+
+int main(){
+    fastio;
+    int t = 1;
+    if (HAS_CASE) cin>>t;
+    while (t--){
+        solve();
+    }
+}
