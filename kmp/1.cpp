962b578e9415316f4991de763006a1708bd6b60f
MartinDAA <martin.alonso.personal@gmail.com>
2026-07-05 19:50:17 -0500

    code: incluidos soluciones de KMP y README actualizado

--- kmp/1.cpp
+++ kmp/1.cpp
@@ -4,17 +4,68 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
 using ll = long long;
 
-const bool HAS_CASE = true; //true if t (test cases) is part of input, false otherwise
+const bool HAS_CASE = false;
 
-void solve(){
-
+vector<int> prefix_function(const string& s) {
+    int n = (int)s.size();
+    vector<int> pi(n, 0);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
 }
 
-int main(){
+void solve() {
+    string s, t;
+    cin >> s >> t;
+
+    int cnt0 = 0, cnt1 = 0;
+    for (char c : s) {
+        if (c == '0') cnt0++;
+        else cnt1++;
+    }
+    int m = t.size();
+    vector<int> pi = prefix_function(t);
+    int overlap = pi[m - 1];
+
+    string ans;
+
+    auto place = [&](char c) -> bool {
+        if (c == '0' && cnt0 > 0) { ans.push_back('0'); cnt0--; return true; }
+        if (c == '1' && cnt1 > 0) { ans.push_back('1'); cnt1--; return true; }
+        return false;
+    };
+
+    for (char c : t) {
+        if (!place(c)) {
+            ans.append(cnt0, '0');
+            ans.append(cnt1, '1');
+            cout << ans << '\n';
+            return;
+        }
+    }
+    while (cnt0 + cnt1 > 0) {
+        for (int i = overlap; i < m; i++) {
+            if (!place(t[i])) {
+                ans.append(cnt0, '0');
+                ans.append(cnt1, '1');
+                cout << ans << '\n';
+                return;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
     fastio;
     int t = 1;
-    if (HAS_CASE) cin>>t;
-    while (t--){
+    if (HAS_CASE) cin >> t;
+    while (t--) {
         solve();
     }
+    return 0;
 }
--- kmp/2.cpp
+++ kmp/2.cpp
@@ -4,17 +4,57 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
 using ll = long long;
 
-const bool HAS_CASE = true; //true if t (test cases) is part of input, false otherwise
+const bool HAS_CASE = false;
 
-void solve(){
-
+vector<int> prefix_function(const string& s) {
+    int n = (int)s.size();
+    vector<int> pi(n, 0);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
 }
 
-int main(){
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    vector<int> pi = prefix_function(s);
+
+    vector<int> cnt(n + 1, 1);
+    
+    for (int i = n - 1; i >= 0; i--) {
+        if (pi[i] > 0) {
+            cnt[pi[i] - 1] += cnt[i];
+        }
+    }
+
+    vector<pair<int, int>> ans;
+    int len = pi[n - 1];
+    while (len > 0) {
+        ans.push_back({len, cnt[len - 1]});
+        len = pi[len - 1];
+    }
+    ans.push_back({n, 1});
+
+    sort(ans.begin(), ans.end());
+
+    cout << ans.size() << "\n";
+    for (auto [l, c] : ans) {
+        cout << l << " " << c << "\n";
+    }
+}
+
+int main() {
     fastio;
     int t = 1;
-    if (HAS_CASE) cin>>t;
-    while (t--){
+    if (HAS_CASE) cin >> t;
+    while (t--) {
         solve();
     }
+    return 0;
 }
